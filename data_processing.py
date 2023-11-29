797ac2fe34b01d4ff2c4ea2fad6158f0c11c5d1d
Natcha Ranchan <natcha.ran@ku.th>
2023-11-29 10:27:20 +0700

    first comit

--- data_processing.py
+++ data_processing.py
@@ -1,8 +1,17 @@
 import csv, os
+class read:
+    def __init__(self,file):
+        self.file = file
+        self.my_csv_data = []
+    def read(self):
+        __location__ = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
+        with open(os.path.join(__location__, self.file)) as f:
+            rows = csv.DictReader(f)
+            for r in rows:
+                self.my_csv_data.append(dict(r))
+        return self.my_csv_data
 class DB:
     def __init__(self):
         self.database = []
@@ -97,6 +106,63 @@ class Table:
             pivot_table.append([item, aggregate_val_list])
         return pivot_table
 
+    def insert_row(self, dict):
+        self.table.append(dict)
+
+    def update_row(self, primary_attribute, primary_attribute_value, update_attribute, update_value):
+        '''
+        This method updates the current value of update_attribute to update_value
+        For example, my_table.update_row('Film', 'A Serious Man', 'Year', '2022') will change the 'Year' attribute for the 'Film'
+        'A Serious Man' from 2009 to 2022
+        '''
+        for item in self.table:
+            if item[primary_attribute] == primary_attribute_value:
+                item[update_attribute] = update_value
+
+
     def __str__(self):
         return self.table_name + ':' + str(self.table)
+movie = read('movies.csv')
+movie_read = movie.read()
+print(movie_read)
+movie_table = Table('movies', movie_read)
+#Find the average value of ‘Worldwide Gross’ for ‘Comedy’ movies
+comedy_filtered = movie_table.filter(lambda x: x['Genre'] == 'Comedy')
+comedy_avg = comedy_filtered.aggregate(lambda x: sum(x)/ len(x), 'Worldwide Gross')
+print(comedy_avg)
 
+#Find the minimum ‘Audience score %’ for ‘Drama’ movies
+drama_filtered = movie_table.filter(lambda x: x['Genre'] == 'Drama')
+drama_lowest_score = drama_filtered.aggregate(lambda x: min(x), 'Audience score %')
+print(drama_lowest_score)
+
+# Count the number of ‘Fantasy’ movie before invoking any of the above two methods
+fantasy_filtered = movie_table.filter(lambda x: x['Genre'] == 'Fantasy')
+fantasy_count = len(fantasy_filtered.table)
+print(fantasy_count)
+
+
+
+dict = {}
+dict['Film'] = 'The Shape of Water'
+dict['Genre'] = 'Fantasy'
+dict['Lead Studio'] = 'Fox'
+dict['Audience score %'] = '72'
+dict['Profitability'] = '9.765'
+dict['Rotten Tomatoes %'] = '92'
+dict['Worldwide Gross'] = '195.3'
+dict['Year'] = '2017'
+movie_table.insert_row(dict)
+# print(movie_table)
+#count the number of ‘Fantasy’ movie again
+
+fantasy_filtered2 = movie_table.filter(lambda x: x['Genre'] == 'Fantasy')
+print(len(fantasy_filtered2.table))
+
+#update the  'Year' for the  'Film' :  'A Serious Man' to '2022'
+
+movie_table.update_row('Film', 'A Serious Man', 'Year', '2022')
+print(movie_table)
+
+# y_table3_filtered = my_table3.filter(lambda x: x['EU'] == 'no').filter(lambda x: float(x['temperature']) < 5.0)
+# print('Max temp:',my_table3EU.aggregate(lambda x: max(x), 'temperature'))
